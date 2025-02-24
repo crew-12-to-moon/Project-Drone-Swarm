c07e645d2939d5189ba724e6f6fd566a69d0f574
Ajay Kanna <119740269+Ajay-Kanna-0231@users.noreply.github.com>
2025-02-24 22:50:22 +0530

    Create apm_launch_2.py

---
+++ Drone-Swarm-2/launch/apm_launch_2.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='mavros',
+            executable='mavros_node',
+            output='screen',
+            parameters=[{
+                'fcu_url': 'udp://127.0.0.1:14560@14565',
+                'gcs_url': '',
+                'target_system_id': 2,
+                'target_component_id': 1,
+                'fcu_protocol': 'v2.0',
+                'namespace': '/drone2'
+            }],
+            remappings=[
+                # If you want to isolate the navigation goal topic
+                ('/move_base_simple/goal', 'move_base_simple/goal'),
+                # If you want to isolate tf
+                ('/tf', 'tf'),
+                ('/tf_static', 'tf_static'),
+                # If you want to isolate the MAVLink sink/source
+                ('/uas1/mavlink_sink', 'uas1/mavlink_sink'),
+                ('/uas1/mavlink_source', 'uas1/mavlink_source'),
+                # Typically do NOT rename /rosout or /parameter_events
+            ],
+            namespace='drone2'
+        ),
+    ])
